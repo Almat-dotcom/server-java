7d3ad018138bba9c52cd888cb7640138fce284f5
almatsagandykov <a.sagandykov@vlife.kz>
2024-03-29 17:49:32 +0500

    pass 6th  stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
@@ -16,73 +17,15 @@ public class Main {
         try {
             serverSocket = new ServerSocket(4221);
             serverSocket.setReuseAddress(true);
-            clientSocket = serverSocket.accept();
-            HttpRequest httpRequest = new HttpRequest(clientSocket.getInputStream());
-            if (matches(httpRequest, "/", "GET")) {
-                byte[] bytes = handleSimpleRequest(httpRequest);
-                writeOutputStream(clientSocket, bytes);
-            } else if (matchesStartsWith(httpRequest, "/echo", "GET")) {
-                byte[] bytes = handleEchoRequest(httpRequest);
-                writeOutputStream(clientSocket, bytes);
-            } else if (matches(httpRequest, "/user-agent", "GET")) {
-                byte[] bytes = handleUserAgentRequest(httpRequest);
-                writeOutputStream(clientSocket, bytes);
-            } else {
-                byte[] bytes = handleNotFoundRequest(httpRequest);
-                writeOutputStream(clientSocket, bytes);
+            while (true) {
+                clientSocket = serverSocket.accept();
+                System.out.println("Accepted new connection");
+                ConnectionHandler connectionHandler =
+                        new ConnectionHandler(clientSocket);
+                new Thread(connectionHandler).start();
             }
-            System.out.println("accepted new connection");
         } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
+            throw new RuntimeException(e);
         }
-    }
-
-    private static byte[] handleEchoRequest(HttpRequest httpRequest) {
-        String body = httpRequest.getPath().split("/echo/")[1];
-
-        HttpResponse httpResponse = new HttpResponse(200, "OK");
-        httpResponse.setBody(body);
-
-        String output = httpResponse.make();
-        System.out.println("The output :: ");
-        System.out.println(output);
-        return output.getBytes(StandardCharsets.UTF_8);
-    }
-
-    private static byte[] handleSimpleRequest(HttpRequest httpRequest) {
-        return "HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8);
-    }
-
-    private static byte[] handleNotFoundRequest(HttpRequest httpRequest) {
-        return "HTTP/1.1 404 Not Found\r\n\r\n".getBytes(StandardCharsets.UTF_8);
-    }
-
-    private static boolean matchesStartsWith(HttpRequest httpRequest, String pathPrefix, String method) {
-        boolean pathMathces = httpRequest.getPath() != null &&
-                httpRequest.getPath().startsWith(pathPrefix);
-        boolean methodMatches = method.equals(httpRequest.getMethod());
-        return pathMathces && methodMatches;
-    }
-
-    private static boolean matches(HttpRequest httpRequest, String path, String method) {
-        boolean pathMathces = path.equals(httpRequest.getPath());
-        boolean methodMatches = method.equals(httpRequest.getMethod());
-        return pathMathces && methodMatches;
-    }
-
-    private static byte[] handleUserAgentRequest(HttpRequest httpRequest) {
-        String userAgent = httpRequest.getHeader(USER_AGENT_HEADER_KEY);
-        HttpResponse httpResponse = new HttpResponse(200, "OK");
-        httpResponse.setBody(userAgent);
-
-        String output = httpResponse.make();
-        System.out.println("The output :: ");
-        System.out.println(output);
-        return output.getBytes(StandardCharsets.UTF_8);
-    }
-
-    private static void writeOutputStream(Socket socket, byte[] bytes)
-            throws IOException {
-        socket.getOutputStream().write(bytes);
     }
 }
